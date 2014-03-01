aaf4c656c7e7716b7f56a38ece0c58314c247f3c
mike <mike@mikes-MacBook-Pro.local>
2014-03-01 14:11:42 -0500

    more refinement. Mongo now can be instructed to create indices.

--- src/main/scala/com/ripjar/spark/process/store/Mongo.scala
+++ src/main/scala/com/ripjar/spark/process/store/Mongo.scala
@@ -1,19 +1,16 @@
 package com.ripjar.spark.process.store
 
 import com.ripjar.spark.data._
-import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.apache.spark.streaming.dstream.DStream
-import java.util.Properties
-import kafka.producer._
-import com.ripjar.spark.job.InstanceConfig
-import com.mongodb.MongoOptions
-import com.mongodb.WriteConcern
-import com.mongodb.DBCollection
-import com.mongodb.DBObject
+import com.mongodb._
 import com.mongodb.util.JSON
 import com.ripjar.spark.process.{TerminalProcessor, Processor}
-import org.apache.spark.streaming.StreamingContext._
+import scala.Some
+import com.ripjar.spark.job.InstanceConfig
+import org.json4s.DefaultFormats
+import java.util
+import scala.collection.mutable
 
 /*
  * Used to store a stream in Mongo
@@ -30,28 +27,38 @@ import org.apache.spark.streaming.StreamingContext._
 object Mongo {
   val logger = LoggerFactory.getLogger(classOf[Mongo])
 
-  var collections: Map[String, DBCollection] = Map.empty
+  val collections: mutable.Map[String, DBCollection] = mutable.Map.empty
 
-  def getDBCollection(config: MongoConfig): DBCollection = {
-    collections.get(config.hash) match {
-      case Some(c) => c
-      case None => {
-        collections.synchronized {
-          collections.get(config.hash) match {
-            case Some(c) => c
-            case None => {
-              val mo = new MongoOptions()
-              mo.connectionsPerHost = config.connectionsPerHost
-              mo.threadsAllowedToBlockForConnectionMultiplier = config.threadsAllowedToBlockForConnectionMultiplier
-              val mongoDB = new com.mongodb.Mongo(config.mongoHost, mo).getDB(config.mongoDbName)
-              val collection = mongoDB.getCollection(config.mongoCollectionName)
-              val concern = WriteConcern.NORMAL
-              concern.continueOnErrorForInsert(true)
-              collection.setWriteConcern(concern)
-              collections += ((config.hash, collection))
-              collection
-            }
-          }
+  def getDBCollection(config: MongoConfig, data: MongoData): DBCollection = {
+    collections.synchronized {
+      collections.get(config.hash) match {
+        case Some(c) => c
+        case None => {
+          val mo = new MongoOptions()
+          mo.connectionsPerHost = config.connectionsPerHost
+          mo.threadsAllowedToBlockForConnectionMultiplier = config.threadsAllowedToBlockForConnectionMultiplier
+
+          val mongoDB = new com.mongodb.Mongo(config.mongoHost, mo).getDB(config.mongoDbName)
+          val collection = mongoDB.getCollection(config.mongoCollectionName)
+          val concern = WriteConcern.NORMAL
+
+          concern.continueOnErrorForInsert(true)
+          collection.setWriteConcern(concern)
+
+          collections += ((config.hash, collection))
+
+          // create the indices
+          data.indices.foreach(index => {
+            val obj = new BasicDBObject()
+
+            index.keys.foreach(key => {
+              obj.put(key, 1)
+            })
+
+            collection.ensureIndex(obj, index.name)
+          })
+
+          collection
         }
       }
     }
@@ -62,8 +69,10 @@ class MongoConfig(val mongoHost: String, val mongoDbName: String, val mongoColle
   val hash = mongoHost + ":" + mongoDbName + ":" + mongoCollectionName + ":" + connectionsPerHost + ":" + threadsAllowedToBlockForConnectionMultiplier
 }
 
-class Mongo(config: InstanceConfig) extends TerminalProcessor with Serializable {
+case class MongoIndices(name: String, keys: List[String])
+case class MongoData(indices: List[MongoIndices])
 
+class Mongo(config: InstanceConfig) extends TerminalProcessor with Serializable {
   val mongoConfig = new MongoConfig(
     config.getMandatoryParameter("host"),
     config.getMandatoryParameter("db"),
@@ -71,22 +80,25 @@ class Mongo(config: InstanceConfig) extends TerminalProcessor with Serializable 
     10,
     100)
 
+  val mongoData = {
+    implicit val formats = DefaultFormats
+
+    config.data.extract[MongoData]
+  }
+
   override def process(stream: DStream[DataItem]): DStream[DataItem] = {
     stream.foreachRDD( rdd => {
       rdd.foreachPartition(iter => {
+        val coll = Mongo.getDBCollection(mongoConfig, mongoData)
+
         iter.foreach( item => {
-          store(item)
+          val dbObject = JSON.parse(item.toString).asInstanceOf[DBObject]
+
+          coll.save(dbObject)
         })
       })
     })
 
     stream
   }
-
-  def store(input: DataItem): DataItem = {
-    val dbObject = JSON.parse(input.toString).asInstanceOf[DBObject]
-    Mongo.getDBCollection(mongoConfig).save(dbObject)
-    input
-  }
-
 }
